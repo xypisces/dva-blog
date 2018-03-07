edb7fb12ccbe94db04de05eea8975734945f760c
xypisces <xypisces@163.com>
2018-03-07 23:08:42 +0800

    'updatelist'

--- src/router.js
+++ src/router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
-import List from './components/List'
+import BlogList from './routes/List/Bloglist';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
-        <Route path="/list" exact component={List} />
+        <Route path="/list" exact component={BlogList} />
       </Switch>
     </Router>
   );
---
+++ src/routes/List/Bloglist.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { connect } from 'dva';
+import { Layout, List, Avatar  } from 'antd';
+import './Bloglist.less';
+const { Header, Footer, Sider, Content } = Layout;
+
+
+class BlogList extends React.Component {
+  render() {
+    return(
+      <div>
+        <Layout>
+          <Header className='title'>
+            <span>xuyu'sblog</span>
+          </Header>
+          <Content className='main'>
+            <ul>
+              <li>
+                <h2 className='main_title'>this is title</h2>
+                <p className='content'>this ithis is contentthis is contentthis is contentthis is contents content!!!</p>
+                <p className='time'>time:1995/04/12</p>
+              </li>
+              <li>
+                <h2 className='main_title'>this is title</h2>
+                <p className='content'>this ithis is contentthis is contentthis is contentthis is contents content!!!</p>
+                <p className='time'>time:1995/04/12</p>
+              </li>
+              <li>
+                <h2 className='main_title'>this is title</h2>
+                <p className='content'>this ithis is contentthis is contentthis is contentthis is contents content!!!</p>
+                <p className='time'>time:1995/04/12</p>
+              </li>
+            </ul>
+          </Content>
+          <Footer className='footer'>
+            <p>物有本末,事有始终,知所先后,则近道亦.</p>
+          </Footer>
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default connect((state)=> {
+  return {
+    loading: state.loading.global,
+    arr: state.example.arr,
+  }
+})(BlogList);
